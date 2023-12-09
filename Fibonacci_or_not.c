ec3b8383fd599b6311a57b3ae37545cbcafefb5f
jayendrasai <147791634+jayendrasai@users.noreply.github.com>
2023-12-08 21:08:57 -0800

    #include<stdio.h> int main() {     int a=0,b=1,c,n,i,f;     scanf("%d",&n);     c=a+b;     while(c<=n)     {         if(c==n)         {             f=1;             break;         }         a=b;         b=c;         c=a+b;     }     if(f==1)     printf("True");     else     printf("False"); }

---
+++ Fibonacci_or_not.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a=0,b=1,c,n,i,f;
+    scanf("%d",&n);
+    c=a+b;
+    while(c<=n)
+    {
+        if(c==n)
+        {
+            f=1;
+            break;
+        }
+        a=b;
+        b=c;
+        c=a+b;
+    }
+    if(f==1)
+    printf("True");
+    else
+    printf("False");
+}
