cecc017adc39717a027177ecb73d3cda0be04246
jayendrasai <147791634+jayendrasai@users.noreply.github.com>
2023-12-16 11:54:25 +0530

    #include<stdio.h> int main() { 	int n,i,l; 	scanf("%d",&n); 	int a[n]; 	for(i=0;i<n;i++) 	{ 		scanf("%d",&a[i]);
    	if(a[i]%2!=0)
    	{
    		l=i;
    	}
    }
    printf("%d ",l);
    	
    }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,l;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	
+	if(a[i]%2!=0)
+	{
+		l=i;
+	}
+}
+printf("%d ",l);
+	
+}
