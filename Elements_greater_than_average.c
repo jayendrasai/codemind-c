112688561dc4a4d6b992d350fd9aeecf236ba3c2
jayendrasai <147791634+jayendrasai@users.noreply.github.com>
2023-12-21 10:27:16 +0530

    #include<stdio.h> int main() { 	int n,i,s; 	scanf("%d",&n); 	int a[n]; 	for(i=0;i<n;i++) 	{ 		scanf("%d",&a[i]); 	} 	s=0; 	for(i=0;i<n;i++) 	{ 		s=s+a[i]; 	} 	int z=s/n; 	int c=0; 	for(i=0;i<n;i++) 	{ 		if(a[i]>=z) 		{ 			c=c+1; 		} 	} 	printf("%d",c); }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,s;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	s=0;
+	for(i=0;i<n;i++)
+	{
+		s=s+a[i];
+	}
+	int z=s/n;
+	int c=0;
+	for(i=0;i<n;i++)
+	{
+		if(a[i]>=z)
+		{
+			c=c+1;
+		}
+	}
+	printf("%d",c);
+}
